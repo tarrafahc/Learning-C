96233e06e88258074f3d9d99c473e1eabd7c99fa
Gustavo Dutra <gobannondutra@gmail.com>
2020-01-02 19:46:20 -0300

    Adaptando código fonte do exercicio 02

--- Assignment-02/assignment2.c
+++ Assignment-02/assignment2.c
@@ -4,10 +4,10 @@ int main(void)
 {
   int integer = 5;
   float floatvar = 3.14;
-  char string[] = "Hello, World!";
+  char charvar[] = 'A';
 
-  printf("%d is an integer!\n", integer);
-  printf("%f is a float!\n", floatvar);
-  printf("%s is a char!\n", string);
+  printf("%d é do tipo inteiro!\n", integer);
+  printf("%f é do tipo float!\n", floatvar);
+  printf("%c é do tipo char!\n", charvar);
 
 }
